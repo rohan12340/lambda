97af0f74a925fe3ae5ca85a69d77dfb95b7c6b58
unknown <rohanmugale45@gmail.com>
2026-07-31 16:33:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+    print("===================================")
+    print("Docker Lambda Application")
+    print("Version : 1")
+    print("Execution Time :", datetime.now())
+    print("===================================")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Docker Lambda Executed Successfully",
+            "time": str(datetime.now())
+        })
+    }
